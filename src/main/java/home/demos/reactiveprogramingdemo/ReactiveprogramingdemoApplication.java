064fb2d9e807091ddb2f2a717b614ed2aba331db
mrnobody3 <zakaria.tourabii@gmail.com>
2020-08-26 18:09:39 +0100

    init the project

---
+++ src/main/java/home/demos/reactiveprogramingdemo/ReactiveprogramingdemoApplication.java
@@ -0,0 +1,13 @@
+package home.demos.reactiveprogramingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveprogramingdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveprogramingdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/home/demos/reactiveprogramingdemo/ReactiveprogramingdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package home.demos.reactiveprogramingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveprogramingdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
