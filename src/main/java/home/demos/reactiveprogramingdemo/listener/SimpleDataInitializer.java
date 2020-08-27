272005f0c7e1cab1e559fc6d927a02116466ccbc
mrnobody3 <zakaria.tourabii@gmail.com>
2020-08-27 18:02:04 +0100

    working with transactional operator

--- src/main/java/home/demos/reactiveprogramingdemo/listener/SimpleDataInitializer.java
+++ src/main/java/home/demos/reactiveprogramingdemo/listener/SimpleDataInitializer.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 
 import home.demos.reactiveprogramingdemo.dao.ReservationRepos;
 import home.demos.reactiveprogramingdemo.entitymodel.Reservation;
+import home.demos.reactiveprogramingdemo.service.ReservationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import reactor.core.publisher.Flux;
@@ -14,10 +15,11 @@ import reactor.core.publisher.Flux;
 @Log4j2
 public class SimpleDataInitializer {
     private final ReservationRepos reservetionRepos;
+    private final ReservationService reservationService;
     
-    @EventListener(ApplicationReadyEvent.class)
+    //@EventListener(ApplicationReadyEvent.class)
     public void ready(){
-        Flux<String> names = Flux.just("zakaria","aymane","saksouka","test");
+        Flux<String> names = Flux.just("Zakaria","Aymane","Saksouka","Test");
         Flux<Reservation> reservations = names.map(name -> new Reservation(null,name));
         // this line will return a Flux<Mono<Reservation>>, so to not avoid
         //that and to get just a Flux<Reserevation> we use the flatMap operator line 26
@@ -34,6 +36,19 @@ public class SimpleDataInitializer {
         // creating a pipeline now that make sure that all the data in our 
         //database will be deleted after that we save the data created up
         //in tha var saved, see below
+        this.reservetionRepos
+        .deleteAll()
+        .thenMany(saved)
+        .thenMany(this.reservetionRepos.findAll())
+        .subscribe(log::info);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void r2dbcReady(){
+        // with transactionalOperator if we creat one name from the list without a first capital later,
+        // nothing will record into the database, and an exception with message "name must start with capital latter"
+        // will raised
+        var saved  = this.reservationService.saveAll("Zakaria","Aymane","Saksouka","Test");
         this.reservetionRepos
         .deleteAll()
         .thenMany(saved)
--- src/main/java/home/demos/reactiveprogramingdemo/service/ReservationService.java
+++ src/main/java/home/demos/reactiveprogramingdemo/service/ReservationService.java
@@ -5,6 +5,7 @@ import org.springframework.util.Assert;
 
 import home.demos.reactiveprogramingdemo.dao.ReservationRepos;
 import home.demos.reactiveprogramingdemo.entitymodel.Reservation;
+import org.springframework.transaction.reactive.TransactionalOperator;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
 
@@ -13,15 +14,18 @@ import reactor.core.publisher.Flux;
 public class ReservationService {
 
     private final ReservationRepos reservationRepos;
+    private final TransactionalOperator transactionalOperator;
 
-    Flux<Reservation> saveAll(String... names){
+    public Flux<Reservation> saveAll(String... names){
+        
         Flux<String> stringFlux = Flux.fromArray(names);
         Flux<Reservation> map = stringFlux.map(name -> new Reservation(null,name));
         Flux<Reservation> fluxReservation = map.flatMap(r -> this.reservationRepos.save(r));
         //the probléme here is, the data would be writing after that he check validity, what if we had two records
         //from 10 writed without a capitale first letter,  so to resolve that we gonna work with reactive
         //transation manager 
-        return fluxReservation.doOnNext(r -> Assert.isTrue(this.isValid(r), "name must start with capitale letter"));
+        return this.transactionalOperator.transactional(
+            fluxReservation.doOnNext(r -> Assert.isTrue(this.isValid(r), "name must start with capitale letter")));
     }
 
     boolean isValid(Reservation r){
