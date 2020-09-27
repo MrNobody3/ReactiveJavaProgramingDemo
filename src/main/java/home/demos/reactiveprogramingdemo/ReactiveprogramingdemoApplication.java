17e41496ff01e4e975348794cc049f7af071c569
Zakaria tourabi <zakaria.tourabii@gmail.com>
2020-09-27 14:55:48 +0100

    Adding two imports (route and ok),
    this imports make routes function good functionning

--- src/main/java/home/demos/reactiveprogramingdemo/ReactiveprogramingdemoApplication.java
+++ src/main/java/home/demos/reactiveprogramingdemo/ReactiveprogramingdemoApplication.java
@@ -29,6 +29,9 @@ import io.r2dbc.spi.ConnectionFactory;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
 @SpringBootApplication
 public class ReactiveprogramingdemoApplication {
 	@Bean
